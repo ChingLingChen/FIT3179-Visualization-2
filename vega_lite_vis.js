5b90a5a4314010a95adeeaf758b5044c620d181c
ChingLingChen <162117245+ChingLingChen@users.noreply.github.com>
2025-10-14 00:21:05 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,14 +1,20 @@
 var vg_1 = "map.json";
 vegaEmbed("#map", vg_1, { actions: false }).then(function(result) {
-  // Vega view instance available as result.view
-}).catch(console.error);
+  console.log("Map loaded");
+}).catch(function(error) {
+  console.error("Error loading map.json:", error);
+});
 
 var vg_2 = "metrics.json";
 vegaEmbed("#metrics", vg_2, { actions: false }).then(function(result) {
-  // Vega view instance available as result.view
-}).catch(console.error);
+  console.log("Metrics loaded");
+}).catch(function(error) {
+  console.error("Error loading metrics.json:", error);
+});
 
 var vg_3 = "two.json";
-vegaEmbed("#scatter-bar", vg_3, { actions: false }).then(function(result) {
-  // Vega view instance available as result.view
-}).catch(console.error);
+vegaEmbed("#precipitation", vg_3, { actions: false }).then(function(result) {
+  console.log("Scatterplot and bar charts loaded");
+}).catch(function(error) {
+  console.error("Error loading two.json:", error);
+});
