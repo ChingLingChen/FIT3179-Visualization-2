29c22023971c4a7984548a37451c41d78f09453d
ChingLingChen <162117245+ChingLingChen@users.noreply.github.com>
2025-10-13 19:32:28 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,7 +3,7 @@ vegaEmbed("#map", vg_1, { actions: false }).then(function(result) {
   // Vega view instance available as result.view
 }).catch(console.error);
 
-var vg_2 = "matrics.json";
-vegaEmbed("#matrics", vg_2, { actions: false }).then(function(result) {
+var vg_2 = "metrics.json";
+vegaEmbed("#metrics", vg_2, { actions: false }).then(function(result) {
   // Vega view instance available as result.view
 }).catch(console.error);
