ee3bc24a59984fd4452c9053a49d38e71fc281d5
ChingLingChen <162117245+ChingLingChen@users.noreply.github.com>
2025-10-14 00:36:23 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -5,16 +5,16 @@ vegaEmbed("#map", vg_1, { actions: false }).then(function(result) {
   console.error("Error loading map.json:", error);
 });
 
-var vg_2 = "metrics.json";
-vegaEmbed("#metrics", vg_2, { actions: false }).then(function(result) {
-  console.log("Metrics loaded");
-}).catch(function(error) {
-  console.error("Error loading metrics.json:", error);
-});
-
-var vg_3 = "two.json";
-vegaEmbed("#precipitation", vg_3, { actions: false }).then(function(result) {
+var vg_2 = "two.json";
+vegaEmbed("#precipitation", vg_2, { actions: false }).then(function(result) {
   console.log("Scatterplot and bar charts loaded");
 }).catch(function(error) {
   console.error("Error loading two.json:", error);
+});
+
+var vg_3 = "metrics.json";
+vegaEmbed("#metrics", vg_3, { actions: false }).then(function(result) {
+  console.log("Metrics loaded");
+}).catch(function(error) {
+  console.error("Error loading metrics.json:", error);
 });
